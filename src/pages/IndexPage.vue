74b7bd780b59fab8c7fd597f40e079eb228ef052
DeWorldR <hanrovth@gmail.com>
2025-07-09 11:11:49 +0700

    Add 'Notify'in quasar.comf.jsversion new

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="q-pa-md" style="max-width: 400px">
-
     <q-form
       @submit="onSubmit"
       @reset="onReset"
@@ -9,32 +8,31 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="ชื่อ-สกุล / Your name *"
+        hint="ชื่อและนามสกุล / Name and surname"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[ val => val && val.length > 0 || 'กรุณาพิมพ์ชื่อ / Please type something']"
       />
 
       <q-input
         filled
         type="number"
         v-model="age"
-        label="Your age *"
+        label="อายุ / Age *"
         lazy-rules
-        :rules="[
-          val => val !== null && val !== '' || 'Please type your age',
-          val => val > 0 && val < 100 || 'Please type a real age'
+        :rules="[ 
+          val => val !== null && val !== '' || 'กรุณาใส่อายุ / Please type your age',
+          val => val > 0 && val < 100 || 'กรุณาใส่อายุจริง / Please type a real age'
         ]"
       />
 
-      <q-toggle v-model="accept" label="I accept th license and terms" />
+      <q-toggle v-model="accept" label="ยอมรับ / I accept the license and terms" />
 
       <div>
-        <q-btn label="Submit" type="submit" color="primary"/>
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        <q-btn label="ยอมรับ / Submit" type="submit" color="primary" />
+        <q-btn label="ยกเลิก / Reset" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
     </q-form>
-
   </div>
 </template>
 
@@ -45,7 +43,6 @@ import { ref } from 'vue'
 export default {
   setup () {
     const $q = useQuasar()
-
     const name = ref(null)
     const age = ref(null)
     const accept = ref(false)
@@ -54,26 +51,23 @@ export default {
       name,
       age,
       accept,
-
       onSubmit () {
         if (accept.value !== true) {
           $q.notify({
             color: 'red-5',
             textColor: 'white',
             icon: 'warning',
-            message: 'You need to accept the license and terms first'
+            message: 'คุณจำเป็นต้องยอมรับ'
           })
-        }
-        else {
+        } else {
           $q.notify({
             color: 'green-4',
             textColor: 'white',
             icon: 'cloud_done',
-            message: 'Submitted'
+            message: 'ข้อมูลได้รับการยืนยัน'
           })
         }
       },
-
       onReset () {
         name.value = null
         age.value = null
