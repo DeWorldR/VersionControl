cee8076aa125d28f61662f74896437e0e27fe21a
DeWorldR <hanrovth@gmail.com>
2025-07-16 10:57:19 +0700

    Edit warning label null

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -21,7 +21,7 @@
         label="อายุ:"
         lazy-rules
         :rules="[ 
-          val => val !== null && val !== '' || 'กรุณาใส่อายุของคุณ / Please type your age',
+          val => val !== null && val !== '' || 'กรุณาใส่อายุของคุณ / Please type your age นะครับ',
           val => val > 0 && val < 100 || 'กรุณาใส่อายุจที่ถูกต้อง / Please type a valid age (0-100) นะจ๊ะ'
         ]"
       />
